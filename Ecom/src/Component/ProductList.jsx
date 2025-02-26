43a8629993b5935159a3e88e9a93f75b7df97a13
rajprajaza <150841876+rajprajaza@users.noreply.github.com>
2025-02-26 16:43:00 +0530

    Update ProductList.jsx

--- Ecom/src/Component/ProductList.jsx
+++ Ecom/src/Component/ProductList.jsx
@@ -11,7 +11,7 @@ function ProductList() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("https://zoroz-backend-w2wh.onrender.com/api/products");
+        const response = await axios.get("https://shoppingweb-uxfk.onrender.com/api/products");
         setProducts(response.data);
       } catch (err) {
         setError("Failed to load products. Please try again later.");
